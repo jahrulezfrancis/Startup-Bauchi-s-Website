bd0ddcb12c906cffe20cb7226fc108036940c1e7
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-30 13:01:46 +0100

    Initial commit

---
+++ src/Components/Pages/Resources/ResourcePage.tsx
@@ -0,0 +1,9 @@
+import { Box, Title } from "@mantine/core";
+
+export default function ResourcesPage() {
+    return (
+        <Box>
+            <Title>Resource Page</Title>
+        </Box>
+    )
+}

957dd96d3ecbf8429548b94dac12fb8a38533db2
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-30 09:35:50 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mantine/core";
+import NavBar from "./Components/NavBar/NavBar";
+
+export default function App() {
+
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
+}

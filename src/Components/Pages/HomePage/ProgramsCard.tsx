bf43ac88d7be0b6a682aa0d1773e53c3e278db56
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-01 19:02:52 +0100

    Added section for icon

--- src/Components/Pages/HomePage/ProgramsCard.tsx
+++ src/Components/Pages/HomePage/ProgramsCard.tsx
@@ -1,9 +1,10 @@
 import { Box, Card, Stack, Title, Text } from "@mantine/core"
-import { MdSell } from "react-icons/md"
+import { ReactElement } from "react";
 
 interface cardProps {
     title: string;
     description: string;
+    icon: ReactElement;
 }
 
 export default function ProgramsCard(props: cardProps) {
@@ -12,7 +13,7 @@ export default function ProgramsCard(props: cardProps) {
             <Card h={250} w={{ base: "400px" }} p={"xl"} shadow="sm" radius="md" withBorder>
                 <Card.Section>
                     <Stack align="center">
-                        <MdSell fontSize={50} />
+                        {props.icon}
                         <Title size={"lg"}>{props.title}</Title>
                         <Text ta="center" size="md">{props.description}</Text>
                     </Stack>

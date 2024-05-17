b332dd3156d475cc8c115b7f5d0abec4ec290d73
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-17 15:43:57 +0100

    Initial commit

---
+++ src/Components/Pages/GetInvolved/GetInvolved.tsx
@@ -0,0 +1,9 @@
+import { Box, Title } from "@mantine/core";
+
+export default function GetInvolvedPage() {
+    return (
+        <Box>
+            <Title>Volunteer Page Page</Title>
+        </Box>
+    )
+}

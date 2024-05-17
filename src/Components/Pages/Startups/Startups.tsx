fe04835b1d44baacc2e0823c78016d553443dbf9
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-17 15:44:05 +0100

    Initial commit

---
+++ src/Components/Pages/Startups/Startups.tsx
@@ -0,0 +1,9 @@
+import { Box, Title } from "@mantine/core";
+
+export default function StartupPage() {
+    return (
+        <Box>
+            <Title>Startup Page you</Title>
+        </Box>
+    )
+}

11f03aaf4cc0dcb0f260e6902c27a400c0b93794
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-17 15:43:44 +0100

    Deleted files

--- src/Components/Pages/News/NewsAndEvents.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Title } from "@mantine/core";
-
-export default function NewsPage() {
-    return (
-        <Box>
-            <Title>New and events page</Title>
-        </Box>
-    )
-}
--- src/Components/Pages/Resources/ResourcePage.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Title } from "@mantine/core";
-
-export default function ResourcesPage() {
-    return (
-        <Box>
-            <Title>Resource Page</Title>
-        </Box>
-    )
-}

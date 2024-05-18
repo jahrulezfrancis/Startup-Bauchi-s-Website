76128147f5bec5cf78ca6d47d50a8245b9574a1c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-18 14:45:13 +0100

    Initial commit

---
+++ src/Components/Pages/Programs/programs.data.ts
@@ -0,0 +1,13 @@
+import SeriesImage from "../../../assets/Images/university.jpg";
+import FoundersImage from "../../../assets/Images/event-venue.jpg";
+import WorkshopImage from "../../../assets/Images/room.jpg";
+import BootcampImage from "../../../assets/Images/bootcamp.jpg";
+
+
+
+export const eventData = [
+    { id: 1, title: "Campus Startup Series", description: "The Campus Startup Series, an exciting event aimed at igniting the entrepreneurial spirit among students. Discover the fundamentals of starting a business, gain insights from successful entrepreneurs, and network with like-minded peers. Whether you're just curious or ready to launch your startup, this series is your gateway to innovation and collaboration.", image: SeriesImage, isDone: true },
+    { id: 2, title: "Bauchi Startup Founders Engagement", description: "Connect, collaborate, and celebrate at the Bauchi Startup Founders Engagement. This networking event brings together the brightest minds in Bauchi's startup scene, providing a platform for founders to share their journeys, exchange ideas, and form valuable partnerships. Join us to foster a supportive community where startups can thrive and prosper.", image: FoundersImage, isDone: true },
+    { id: 3, title: "Ideation Workshop", description: "Unleash your creativity at our Ideation Workshop! This event is dedicated to helping aspiring entrepreneurs and innovators brainstorm, refine, and validate their business ideas. With guidance from industry experts and collaborative activities, you'll leave with a clear roadmap to bring your vision to life. Come with an idea, leave with a plan!", image: WorkshopImage, isDone: false },
+    { id: 4, title: "Startup Bootcamp Bauchi", description: "Dive into the Startup Bootcamp Bauchi, a program designed to equip you with the essential skills and knowledge to kickstart your entrepreneurial journey. Through hands-on workshops, expert-led sessions, and collaborative projects, you'll learn to turn your innovative ideas into viable businesses. Let's build the future of Bauchi's startup ecosystem together!", image: BootcampImage, isDone: false },
+]

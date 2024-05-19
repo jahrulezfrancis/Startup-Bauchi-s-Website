35d2b4b238177f4f72493a0e9b953e7159ee0ef9
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-19 18:47:55 +0100

    Initial commit

---
+++ src/Components/Pages/News/BlogPostCard.tsx
@@ -0,0 +1,40 @@
+import { Group, Card, Button, Image, Text } from "@mantine/core"
+import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+
+
+interface cardProps {
+    title: string;
+    description: string;
+    icon?: ReactElement;
+    postLink: string;
+}
+
+export default function BlogPostCard(props: cardProps) {
+    return (
+        <Card w={"500px"} shadow="sm" padding="lg" radius="md" withBorder={false}>
+            <Card.Section>
+                <Image
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                    height={160}
+                    alt="Norway"
+                />
+            </Card.Section>
+
+            <Group justify="space-between" mt="md" mb="xs">
+                <Text fw={500}>{props.title}</Text>
+                {/* <Badge color="pink">On Sale</Badge> */}
+            </Group>
+
+            <Text size="sm" c="dimmed">
+                {props.description}
+            </Text>
+
+            <Link to={props.postLink}>
+                <Button color="blue" fullWidth mt="md" radius="md">
+                    Learn More
+                </Button>
+            </Link>
+        </Card>
+    )
+}

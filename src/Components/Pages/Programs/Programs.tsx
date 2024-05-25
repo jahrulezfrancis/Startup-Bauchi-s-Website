ee6625c704e86d50461dc96572f2b7cfa3400a20
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-25 19:42:05 +0200

    Updating layout and fixing responsiveness

--- src/Components/Pages/Programs/Programs.tsx
+++ src/Components/Pages/Programs/Programs.tsx
@@ -18,7 +18,7 @@ export default function ProgramsPage() {
             <Stack>
                 <Title mt={50} ta={"center"}>Program highlight</Title>
                 <Text mb={50} style={{ alignSelf: "center" }} ta="center" w={{ base: "80%", md: "70%", lg: "70%" }}>Through our array of programs, we invite you to explore a landscape of opportunities designed to nurture creativity, ignite ambition, and drive sustainable growth. Whether you're an aspiring entrepreneur seeking guidance on launching your first venture, a startup poised for exponential growth, or an established organization aiming to cultivate innovation within your ranks, our programs are tailored to meet your unique needs and aspirations.</Text>
-                <Stack style={{ borderRadius: 10 }} mx={50} p={30} bg={ashBg}>
+                <Stack style={{ borderRadius: 10 }} mx={{ base: 0, md: 10, lg: 50 }} p={{ base: 10, md: 20, lg: 30 }} bg={ashBg}>
                     <Title ta="center">What we do at Startup Bauchi</Title>
                     <Text ta="center">We offer a range of programs and initiatives to support entrepreneurs and startups at various stages of development, they include:</Text>
 

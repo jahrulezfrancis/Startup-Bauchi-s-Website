e06283ebd72deba61dcb140dcc432b95af49ac5c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-27 16:53:11 +0100

    Implementing email sending on form submission

--- src/Components/Pages/GetInvolved/Forms/Volunteer.form.tsx
+++ src/Components/Pages/GetInvolved/Forms/Volunteer.form.tsx
@@ -1,9 +1,12 @@
 import { Box, Stack, Flex, Button, Space, TextInput, Select } from "@mantine/core";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { FaHandsHelping } from "react-icons/fa";
+import emailjs from "@emailjs/browser"
+import { notifications } from "@mantine/notifications";
 
 export default function VolunteerSignUpForm() {
 
+    const [loading, setIsloading] = useState(false)
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -11,34 +14,62 @@ export default function VolunteerSignUpForm() {
         role: "",
     });
 
-
+    const emailjs_pubkey = import.meta.env.VITE_EMAILJS_PUBKEY
+    const serviceKey = import.meta.env.VITE_EMAIL_JS_SERVICE
+    const templateId = import.meta.env.VITE_EMAILJS_INDIVIDUAL_TEMP
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
         setFormData({ ...formData, [name]: value });
     };
 
+    const templateSheme = {
+        from_name: formData.name,
+        from_email: formData.email,
+        from_phone_number: formData.phone,
+        to_name: "Startup Bauchi Admin",
+        person_role: formData.role
+    }
+
+    function clearForm() {
+        setFormData({ name: "", email: "", phone: "", role: "" })
+    }
 
     const handleFormSubmit = (event: FormEvent) => {
         event.preventDefault();
+        setIsloading(true)
         const { name, email, phone, role } = formData;
         if (name && email && phone && role) {
+            setIsloading(true)
             // handle form submission
-            alert("Thank you for signing up to volunteer!");
+            emailjs.send(serviceKey, templateId, templateSheme, emailjs_pubkey)
+                .then(
+                    () => {
+                        setIsloading(false)
+                        clearForm()
+                        return notifications.show({ title: "Application sent successfully", message: "Your Application has been received successfully. Our team will get back to you within 48 hours. Regards", color: "teal", })
+                    },
+                    (error) => {
+                        console.log('FAILED...', error.text);
+                        setIsloading(false)
+                        return notifications.show({ title: "Registration Failed", message: "Your Application was not sent successfully. Please try again", color: "teal" })
+                    },
+                );
         } else {
             alert("Please fill out all fields.");
+            setIsloading(false)
         }
     };
 
     return (
         <Box px={10} pt={20}>
-        <Flex w={"100%"} direction={"column"} justify="center" align={{ base: "", md: "center" }}>
+            <Flex w={"100%"} direction={"column"} justify="center" align={{ base: "", md: "center" }}>
                 <Flex justify="center" gap={20} wrap="wrap">
                     <FaHandsHelping fontSize={80} />
                 </Flex>
                 {/* <Space h={30} /> */}
                 <form onSubmit={handleFormSubmit}>
-                    <Stack align="center" w={{ base: "100%", md: "100%", lg: "500px" }}>
+                    <Stack align="center" w={{ base: "90vw", md: "80vw", lg: "500px" }}>
 
                         <TextInput w={"100%"} radius={10} label="Full name" size="md" name="name" value={formData.name} onChange={handleInputChange}
                             placeholder="Your Name" required />
@@ -62,7 +93,7 @@ export default function VolunteerSignUpForm() {
                             onChange={(_value, option) => setFormData({ ...formData, role: option.value })}
                             required
                         />
-                        <Button w={"100%"} radius={10} mt={30} size="md" type="submit">Submit Application</Button>
+                        <Button loading={loading} w={"100%"} radius={10} mt={30} size="md" type="submit">Submit Application</Button>
                     </Stack>
                 </form>
             </Flex>

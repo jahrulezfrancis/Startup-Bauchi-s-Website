741c0490cc9c17a2b2c6ca4a59e6467064bf3f5e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-06 00:07:00 +0100

    Updating programs

--- src/Components/Renderers/ProgramRenderers.tsx
+++ src/Components/Renderers/ProgramRenderers.tsx
@@ -14,12 +14,12 @@ export default function ProgramsRenderer(props: elementProps) {
     return (
         <Stack>
             <Flex justify={"center"} gap={50} wrap={"wrap"}>
-                <ProgramsCard index={2} icon={<PiPresentationLight color={secondaryColor} fontSize={50} />} title="Startup Showcases" description="Opportunities for startups to showcase their products, services, and innovations to potential investors, customers, and partners." />
+                <ProgramsCard index={2} icon={<PiPresentationLight color={secondaryColor} fontSize={50} />} title="Startup Bootcamp" description="Intensive bootcamps that will quickly develop practical skills for the startup world, providing immersive, and hands-on experience" />
 
                 <ProgramsCard index={2} icon={<GiTeacher color={secondaryColor} fontSize={50} />} title="Workshops and Training" description="Educational programs covering various aspects of entrepreneurship, business development, technology, and innovation." />
 
                 <ProgramsCard icon={<FaConnectdevelop color={secondaryColor} fontSize={50} />} title="Networking Events" description="Regular meetups, pitch nights, and networking sessions to connect entrepreneurs, investors, and industry professionals." />
-                
+
                 {props.showAll && <ProgramsCard icon={<GrGrow color={secondaryColor} fontSize={50} />} title="Incubation" description="Support for startups in their early stages, including workspace, mentorship, and access to funding opportunities." />}
             </Flex>
         </Stack>

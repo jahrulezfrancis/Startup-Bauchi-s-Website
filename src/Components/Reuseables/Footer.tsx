ad0d29d53c18dfd41eeff076a6d509dafb323662
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-06 21:29:05 +0100

    Updating footer layout and styling

--- src/Components/Reuseables/Footer.tsx
+++ src/Components/Reuseables/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, Stack, Text, Title, Tooltip } from "@mantine/core"
+import { Divider, Flex, Grid, Stack, Text, Title, Tooltip } from "@mantine/core"
 import { navlinks } from "../Utils/navlinks"
 import NewsLetterSection from "./NewsLetterSection"
 import { Link, NavLink } from "react-router-dom"
@@ -7,34 +7,20 @@ import { FaXTwitter } from "react-icons/fa6"
 import { secondaryColor } from "./Color"
 
 export default function FooterSection() {
-    // function FooterIconComponent() {
-    //     return (
-    //         <Tooltip position="bottom" label="Join Telegram commnity">
-    //             <Flex align={"center"} gap={5}>
-    //                 <Link to={""}>
-    //                    <FaTelegram color={secondaryColor} fontSize={30} />
-    //                 </Link>
-    //                 <Text>{siteName}</Text>
-    //             </Flex>
-    //         </Tooltip>
-    //     )
-    // }
+
     return (
         <>
-            <Grid px={20} py={"10px"}>
-                <Grid.Col ml={"auto"} span={{ base: 12, md: 6, lg: 3 }}>
-                    <Text mt={10} fs={"italic"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolorum velit expedita ratione perferendis quia itaque nulla! Doloremque magni ullam quam qui consequuntur saepe neque inventore, quo, labore molestias nemo.</Text>
-                </Grid.Col>
-                <Grid.Col mr={"auto"} span={{ base: 12, md: 6, lg: 3 }}>
-                    <Stack justify='start' align='start'>
-                        <Title c={"black"} ta={"start"}>Engage & Interact</Title>
-                        <Stack gap={10} justify="center">
-                            <Tooltip position="bottom" label="Join Telegram commnity">
+            <Grid gutter={{ base: "xl" }} style={{ justifySelf: "center" }} mx={"lg"} px={20} py={"20px"}>
+                <Grid.Col  span={{ base: 12, md: 6, lg: 3 }}>
+                    <Stack justify='start'>
+                        <Title fw={500} c={"black"} ta={"start"}>Engage & Interact</Title>
+                        <Stack gap={1} justify="center">
+                            <Tooltip mt={0} position="bottom" label="Join Telegram commnity">
                                 <Flex align={"center"} gap={5}>
                                     <Link to={""}>
                                         <FaTelegram color={secondaryColor} fontSize={30} />
                                     </Link>
-                                    <Text>Telegram</Text>
+                                    <Text >Telegram</Text>
                                 </Flex>
                             </Tooltip>
 
@@ -43,7 +29,7 @@ export default function FooterSection() {
                                     <Link to={""}>
                                         <FaWhatsapp color={secondaryColor} fontSize={30} />
                                     </Link>
-                                    <Text>Whatsapp</Text>
+                                    <Text >Whatsapp</Text>
                                 </Flex>
                             </Tooltip>
                             <Tooltip position="bottom" label="Join X community">
@@ -51,7 +37,7 @@ export default function FooterSection() {
                                     <Link to={""}>
                                         <FaXTwitter color={secondaryColor} fontSize={30} />
                                     </Link>
-                                    <Text>X </Text>
+                                    <Text >X </Text>
                                 </Flex>
                             </Tooltip>
                         </Stack>
@@ -59,12 +45,12 @@ export default function FooterSection() {
                 </Grid.Col>
 
                 <Grid.Col mx={"auto"} span={{ base: 12, md: 6, lg: 3 }}>
-                    <Title mb={10} ta={"start"}>Quick links</Title>
+                    <Title fw={500} mb={5} ta={"start"}>Quick links</Title>
                     <Flex align={"start"} justify={"start"} direction={"column"} gap={10}>
                         {navlinks.map((item) => {
                             return (
                                 <NavLink className={item.path === location.pathname ? "active" : ""} style={{ textDecoration: "none" }} key={item.id} to={item.path}>
-                                    <Text ta={"start"} fw={700} c={secondaryColor} style={{ textDecoration: "none" }} size='18px' >{item.name}</Text>
+                                    <Text ta={"start"} td={"underline"} c={"black"} size='18px' >{item.name}</Text>
                                 </NavLink>
                             )
                         })}
@@ -75,6 +61,7 @@ export default function FooterSection() {
                     <NewsLetterSection />
                 </Grid.Col>
             </Grid>
+            <Divider />
             <Text ta={"center"} py={"20px"} size={"md"}>© 2024 Startup Bauchi</Text>
         </>
     )

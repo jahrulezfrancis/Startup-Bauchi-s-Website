d003e5f50cdf384c1dfec3d13dd69d6701fd9bae
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-07 03:59:38 +0100

    Initial commit

---
+++ src/Components/Pages/Programs/SignupPage.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mantine/core";
+import ProgramSignUpForm from "../GetInvolved/Forms/ProgramSignup";
+
+export default function ProgramsSignupPage() {
+    return (
+        <Box>
+            <ProgramSignUpForm />
+        </Box>
+    )
+}

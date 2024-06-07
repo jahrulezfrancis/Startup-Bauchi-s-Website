7de510c4ebbbdbbc299e4ecbd296fb81218042a1
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-07 12:47:35 +0100

    Initial commit

---
+++ src/Components/Utils/envExports.ts
@@ -0,0 +1,7 @@
+const firebase_api_key = import.meta.env.VITE_FIREBASE_API_KEY;
+const firebase_app_id = import.meta.env.VITE_FIREBASE_APP_ID;
+const firebase_project_id = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const firebase_messaging_sender_id = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+
+
+export { firebase_api_key, firebase_app_id, firebase_project_id, firebase_messaging_sender_id }

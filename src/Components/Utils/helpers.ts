d74aabd56c5a1d82f7b31a839ed54b921e3f38c3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-09 13:38:59 +0100

    Initial commit

---
+++ src/Components/Utils/helpers.ts
@@ -0,0 +1,11 @@
+import { CollectionReference } from "firebase/firestore";
+
+
+
+export function FetchDataFromCollection(collection: CollectionReference){
+
+}
+
+export function UploadToCollection(data){
+
+}

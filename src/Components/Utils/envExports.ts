2c2df8d0a71eadac3f7667952fc4576536a69b20
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-09 22:54:40 +0100

    adding mail export values

--- src/Components/Utils/envExports.ts
+++ src/Components/Utils/envExports.ts
@@ -4,4 +4,10 @@ const firebase_project_id = import.meta.env.VITE_FIREBASE_PROJECT_ID;
 const firebase_messaging_sender_id = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
 
 
-export { firebase_api_key, firebase_app_id, firebase_project_id, firebase_messaging_sender_id }
+const emailjs_pubkey = import.meta.env.VITE_EMAILJS_PUBKEY;
+const serviceKey = import.meta.env.VITE_EMAIL_JS_SERVICE;
+const templateId = import.meta.env.VITE_EMAILJS_STARTUP_TEMP;
+const emailReceiver = import.meta.env.VITE_EMAIL_RECEIVER
+
+
+export { firebase_api_key, firebase_app_id, firebase_project_id, firebase_messaging_sender_id , emailReceiver, emailjs_pubkey, templateId, serviceKey}

f5faa86e99cd12457da2716bf7f53bce34d34fe6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-09 21:49:05 +0100

    Updating helpers

--- src/Components/Utils/helpers.ts
+++ src/Components/Utils/helpers.ts
@@ -1,11 +1,22 @@
-import { CollectionReference } from "firebase/firestore";
+import { CollectionReference, getDocs } from "firebase/firestore";
 
 
 
-export function FetchDataFromCollection(collection: CollectionReference){
+export async function FetchDataFromCollection(collection: CollectionReference) {
+    const programsSnapshot = await getDocs(collection);
+
+    const programData = programsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        startupName: doc.data().startupName,
+        startupLogo: doc.data().startupLogo,
+        description: doc.data().description,
+        // ...doc.data(),
+    }));
+    return programData
 
 }
 
-export function UploadToCollection(data){
+export function UploadToCollection() {
+    
 
 }

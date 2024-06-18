d498ebfdfe079f7fb15b9b4b0b73071f5bbb0c61
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-18 01:15:53 +0100

    Added default button color

--- src/Components/Reuseables/SolidButton.tsx
+++ src/Components/Reuseables/SolidButton.tsx
@@ -1,14 +1,16 @@
 import { Button, ButtonProps } from "@mantine/core";
+import { secondaryColor } from "./Color";
 
 interface btnProps extends ButtonProps {
     buttonText: string;
     onClick?: () => void;
     type?: "submit" | "button"
+    buttonCol?: string
 }
 
 export default function CustomSolidButton(props: btnProps) {
 
     return (
-        <Button type={props.type} {...props} onClick={props.onClick} fullWidth={props.fullWidth} style={props.style}>{props.buttonText}</Button>
+        <Button type={props.type} {...props} color={props.color ? props.buttonCol : secondaryColor} onClick={props.onClick} fullWidth={props.fullWidth} style={props.style}>{props.buttonText}</Button>
     )
 }

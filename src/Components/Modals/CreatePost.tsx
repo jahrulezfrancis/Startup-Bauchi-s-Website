cd6d1aead3416dc87d2fc03d49049cd3213e64a3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-06-20 14:00:14 +0100

    Updating post creating function

--- src/Components/Modals/CreatePost.tsx
+++ src/Components/Modals/CreatePost.tsx
@@ -1,38 +1,103 @@
 import { useDisclosure } from '@mantine/hooks';
-import { FileInput, Modal, Stack, TextInput, Textarea } from '@mantine/core';
+import { FileInput, Loader, Modal, Stack, TextInput, Textarea } from '@mantine/core';
 import CustomSolidButton from '../Reuseables/SolidButton';
 import { Text, Group, rem } from '@mantine/core';
 import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
 import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE, } from '@mantine/dropzone';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { IoCreate } from "react-icons/io5";
-import { useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
+import { addDoc } from 'firebase/firestore';
+import { postRef, storage } from '../Utils/firebase.config';
+import { notifications } from '@mantine/notifications';
 
 
-interface imageType {
-    images: string[]
+
+interface UploadedFile extends File {
+    preview: string;
 }
 
 export default function CreateNewPost() {
     const [opened, { open, close }] = useDisclosure(false);
 
-    const [images, setImages] = useState<imageType[]>()
-
-
     const [postDetails, setPostDetails] = useState({
         title: "",
         content: "",
         featuredImage: undefined,
-        // images: [],
         author: "",
         createdAt: new Date().toUTCString()
     })
+
+console.log(postDetails.author)
+
+    // State to hold the uploaded files
+    const [files, setFiles] = useState<UploadedFile[]>([]);
+    const [uploading, setUploading] = useState(false);
+    const [disabled, setDisabled] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    // Handle files drop
+    const handleDrop = (droppedFiles: File[]) => {
+        const filesWithPreview = droppedFiles.map(file => ({
+            ...file,
+            preview: URL.createObjectURL(file),
+        })) as UploadedFile[];
+        setFiles((currentFiles) => [...currentFiles, ...filesWithPreview]);
+    };
+
+    const documentData = {
+        title: postDetails.title,
+        content: postDetails.content,
+        featuredImage: postDetails.featuredImage,
+        createdAt: postDetails.createdAt,
+        author: "admin",
+        // images: images
+
+    }
+   
+
+    // Function to upload files to Firebase
+    const uploadFiles = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setUploading(true);
+        setError(null);
+        try {
+            const uploadPromises = files.map(async (file) => {
+                const storageRef = ref(storage, `uploads/${file.name}`);
+                const uploadTask = uploadBytesResumable(storageRef, file);
+
+                await uploadTask;
+                const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+                return downloadURL;
+            });
+
+            const downloadURLs = await Promise.all(uploadPromises);
+            console.log('Uploaded files:', downloadURLs);
+
+            addDoc(postRef, { ...documentData, images: downloadURLs },)
+                .then(() => {
+                    notifications.show({ title: "Post added successfully", message: "Your post was successfully updated" })
+                }).catch((err) => {
+                    notifications.show({ title: "Failed to add post", message: "An error occured: " + err + " and post upload could not be completed" })
+                })
+            // Here you can handle the downloadURLs, e.g., save them to your database along with the post
+
+            // Reset files after upload
+            setFiles([]);
+        } catch (err) {
+            setError('Failed to upload files');
+            console.error(err);
+        } finally {
+            setUploading(false);
+        }
+    };
 
 
     function DropzoneButton(props: Partial<DropzoneProps>) {
 
         return (
             <Dropzone
-                onDrop={(files) => console.log('accepted files', files.map((item) => setImages(item.path)))}
+                onDrop={handleDrop}
                 onReject={(files) => console.log('rejected files', files)}
                 maxSize={5 * 1024 ** 2}
                 accept={IMAGE_MIME_TYPE}
@@ -82,15 +147,26 @@ export default function CreateNewPost() {
                     backgroundOpacity: 0.55,
                     blur: 3,
                 }}>
-                <Stack>
-                    <TextInput onChange={(e) => setPostDetails((prevValue) => ({ ...prevValue, title: e.target.value }))} name='title' value={postDetails.title} label="Post Title" placeholder='Post title' />
-                    <Textarea onChange={(e) => setPostDetails((prevValue) => ({ ...prevValue, content: e.target.value }))} name='content' value={postDetails.content} resize="vertical" label="Post content" placeholder='post content ' />
-                    <FileInput name={"featuredImage"} value={postDetails.featuredImage} multiple={false} label="Featured Image" placeholder="Select featured image" accept={IMAGE_MIME_TYPE.toString()} />
-                    <DropzoneButton />
-                </Stack>
+                <form onSubmit={uploadFiles}>
+                    <Stack>
+                        <TextInput onChange={(e) => setPostDetails((prevValue) => ({ ...prevValue, title: e.target.value }))} name='title' value={postDetails.title} label="Post Title" placeholder='Post title' />
+                        <Textarea onChange={(e) => setPostDetails((prevValue) => ({ ...prevValue, content: e.target.value }))} name='content' value={postDetails.content} resize="vertical" label="Post content" placeholder='post content ' />
+                        <FileInput name={"featuredImage"} value={postDetails.featuredImage} multiple={false} label="Featured Image" placeholder="Select featured image" accept={IMAGE_MIME_TYPE.toString()} />
+                        <DropzoneButton />
+                        <Group justify='space-between'>
+                            <CustomSolidButton color='red' variant='outline' buttonText='Cancel' />
+                            {uploading ?
+                                <Loader size={"sm"} />
+                                :
+                                <CustomSolidButton disabled={disabled} type="submit" buttonText='Publish post' />
+                            }
+                        </Group>
+                    </Stack>
+                </form>
 
-            </Modal>
+            </Modal >
             <Stack p={20}>
+
                 <CustomSolidButton w={"max-content"} h={"40px"} fullWidth={false} leftSection={<IoCreate fontSize={20} style={{ alignSelf: "center" }} />} buttonText='Create post' onClick={open} />
             </Stack>
         </>

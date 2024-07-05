c9bbe683a238fc6594601b7a516c39001a72975f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-07-05 10:51:13 +0100

    Removing blog page

--- src/Components/Utils/navlinks.ts
+++ src/Components/Utils/navlinks.ts
@@ -2,7 +2,7 @@ export const navlinks = [
     // { id: 1, path: "/", name: "Home" },
     { id: 2, path: "/about", name: "About us" },
     { id: 3, path: "/programs", name: "Programs" },
-    { id: 4, path: "/blog", name: "Blog" },
+    // { id: 4, path: "/blog", name: "Blog" },
     { id: 6, path: "/get-involved", name: "Get Involved" },
     { id: 7, path: "/join-startup", name: "Join startup community" },
 

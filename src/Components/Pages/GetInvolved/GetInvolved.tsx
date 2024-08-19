88bc46d2a9ae0d08fbf3ac6e2755c8cfc8b6e9d4
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-08-19 13:07:47 +0100

    Updating content

--- src/Components/Pages/GetInvolved/GetInvolved.tsx
+++ src/Components/Pages/GetInvolved/GetInvolved.tsx
@@ -14,17 +14,17 @@ export default function GetInvolvedPage() {
     return (
         <Box>
             <Stack align="center" p={10}>
-                <Title ta="center">Get Involved</Title>
-                <Text mb={20} ta="center" w={{ base: "100%", md: "70%", lg: "800px" }}>
-                    Join us in making a difference. Whether you're looking to volunteer your time, share your expertise, or support our initiatives, there are many ways to get involved with Startup Bauchi.
+                <Title ta="center">Join us in making a difference</Title>
+                <Text lh={2} mb={20} ta="center" w={{ base: "100%", md: "70%", lg: "800px" }}>
+                    Whether you're looking to volunteer your time, share your expertise, or support our initiatives, there are many ways to get involved with Startup Bauchi.
                 </Text>
             </Stack>
-            <Tabs p={10} value={activeTab} onChange={setActiveTab}>
+            <Tabs  p={10} value={activeTab} onChange={setActiveTab}>
                 <Tabs.List justify="center">
                     <Tabs.Tab value="first">
                         <Text>Sign up as a partner</Text>
                     </Tabs.Tab>
-                    
+
                     <Tabs.Tab value="second">
                         <Text>Sign up as a volunteer</Text>
                     </Tabs.Tab>

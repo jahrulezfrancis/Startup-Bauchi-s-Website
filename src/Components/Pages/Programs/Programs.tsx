02aef4183b3c072335e12c501f46ca0aafa6622f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-08-19 13:08:46 +0100

    Updating content and layout

--- src/Components/Pages/Programs/Programs.tsx
+++ src/Components/Pages/Programs/Programs.tsx
@@ -23,14 +23,11 @@ export default function ProgramsPage() {
         <Box maw={"100vw"} pt={20}>
             <FadeInOnScrollAnimationContainer children={
                 <Stack style={{ animation: "" }}>
-                    <Title ta={"center"}>Program highlight</Title>
-                    <Text mb={50} style={{ alignSelf: "center" }} ta="center" w={{ base: "80%", md: "70%", lg: "70%" }}>Through our array of programs, we invite you to explore a landscape of opportunities designed to nurture creativity, ignite ambition, and drive sustainable growth. Whether you're an aspiring entrepreneur seeking guidance on launching your first venture, a startup poised for exponential growth, or an established organization aiming to cultivate innovation within your ranks, our programs are tailored to meet your unique needs and aspirations.</Text>
-                    <Stack style={{ borderRadius: 10 }} mx={{ base: 0, md: 10, lg: 50 }} p={{ base: 10, md: 20, lg: 30 }}>
-                        <Title ta="center">What we do at Startup Bauchi</Title>
-                        <Text ta="center">We offer a range of programs and initiatives to support entrepreneurs and startups at various stages of development, they include:</Text>
-                    </Stack>
+                    <Title ta={"center"}>Programs and events within Startup Bauchi's Eco-System</Title>
+                    <Text mb={50} lh={2} style={{ alignSelf: "center" }} ta="center" w={{ base: "80%", md: "70%", lg: "70%" }}>Through our array of programs, we invite you to explore a landscape of opportunities designed to nurture creativity, ignite ambition, and drive sustainable growth. Whether you're an aspiring entrepreneur seeking guidance on launching your first venture, a startup poised for exponential growth, or an established organization aiming to cultivate innovation within your ranks, our programs are tailored to meet your unique needs and aspirations.</Text>
                 </Stack>
             } />
+
             <ProgramsRenderer showHoverContent showAll />
 
             <Space h={100} />

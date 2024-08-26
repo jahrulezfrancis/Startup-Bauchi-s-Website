377eda5b915530418bc7aaebd9a09f9f1c9f01cd
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-08-26 22:14:34 +0100

    Adding background to sign up form

--- src/Components/Pages/Startups/Startups.tsx
+++ src/Components/Pages/Startups/Startups.tsx
@@ -1,15 +1,16 @@
-import { Box, Stack, Title } from "@mantine/core";
+import { Title } from "@mantine/core";
 import StartupSignUpForm from "../GetInvolved/Forms/Startup.form";
-
+import FormOverlayContainer from "../../Reuseables/FormOverlayContainer";
 
 export default function StartupPage() {
 
     return (
-        <Box pt={20}>
-            <Stack>
+        <FormOverlayContainer content={
+            <>
                 <Title ta={"center"}>Join our Community</Title>
                 <StartupSignUpForm buttonText={"Join now"} showIcon={false} formPosition="center" />
-            </Stack>
-        </Box>
+            </>
+        } />
+
     );
 }
